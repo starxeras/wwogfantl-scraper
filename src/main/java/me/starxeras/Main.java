4bc6c39ff7a664590c1cc708cd5ee02e911e9ad8
starxeras <starxeras@gmail.com>
2023-12-14 21:16:05 -0600

    Added JDOC

--- src/main/java/me/starxeras/Main.java
+++ src/main/java/me/starxeras/Main.java
@@ -3,6 +3,6 @@ package me.starxeras;
 public class Main {
     public static void main(String[] args) {
         Chapter chapter = new Chapter("https://wwogfantl.bearblog.dev/922/");
-        System.out.println(chapter.getContent());
+        System.out.println(chapter.getFormattedContent());
     }
 }
