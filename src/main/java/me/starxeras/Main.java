14e57e9fbce1ea50847030f8d1c37f9c356422eb
starxeras <starxeras@gmail.com>
2023-12-14 21:03:30 -0600

    Init

---
+++ src/main/java/me/starxeras/Main.java
@@ -0,0 +1,8 @@
+package me.starxeras;
+
+public class Main {
+    public static void main(String[] args) {
+        Chapter chapter = new Chapter("https://wwogfantl.bearblog.dev/922/");
+        System.out.println(chapter.getTitle());
+    }
+}
